85860fb97e81b7f069091ebede69f2211a6a472d
gseoeun2079-cell <gseoeun2079@gmail.com>
2026-02-13 15:32:51 +0900

    선택지에 상관없음 삭제
    상관없음까지 하면 200개가 넘는 경우의수로 잠시 삭제

--- project.py
+++ project.py
@@ -21,12 +21,12 @@ if st.session_state.page == '첫화면':
 
 elif st.session_state.page == '선택화면':
     st.header("질문에 답해주세요")
-    selected_option1 = st.selectbox("한식과 중식, 양식 중 골라주세요",("한식","중식","양식","상관없음"))
-    selected_option2 = st.selectbox("국물이 있는 음식을 원하시나요?",("국물O","국물X","상관없음"))
+    selected_option1 = st.selectbox("한식과 중식, 양식 중 골라주세요",("한식","중식","양식"))
+    selected_option2 = st.selectbox("국물이 있는 음식을 원하시나요?",("국물O","국물X"))
     selected_option3 = st.selectbox("매운 음식을 원하시나요?",("매운맛O","매운맛X","상관없음"))
-    selected_option4 = st.selectbox("시원한 음식을 원하시나요, 따뜻한 음식을 원하시나요?",("시원한 음식","따뜻한 음식","상관없음"))
-    selected_option5 = st.selectbox("밥 종류의 음식을 원하시나요, 면 종류의 음식을 원하시나요?",("밥 종류의 음식","면 종류의 음식","상관없음"))
-    selected_option6 = st.selectbox("기름진 음식을 원하시나요, 깔끔한 음식을 원하시나요?",("기름진 음식","깔끔한 음식","상관없음"))
+    selected_option4 = st.selectbox("시원한 음식을 원하시나요, 따뜻한 음식을 원하시나요?",("시원한 음식","따뜻한 음식"))
+    selected_option5 = st.selectbox("밥 종류의 음식을 원하시나요, 면 종류의 음식을 원하시나요?",("밥 종류의 음식","면 종류의 음식"))
+    selected_option6 = st.selectbox("기름진 음식을 원하시나요, 깔끔한 음식을 원하시나요?",("기름진 음식","깔끔한 음식"))
     a = {'selected_option1':selected_option1,'selected_option2':selected_option2,'selected_option3':selected_option3,'selected_option4':selected_option4,'selected_option5':selected_option5,'selected_option6':selected_option6}
     if st.button("선택완료"):
         st.session_state.page = '결과화면'
